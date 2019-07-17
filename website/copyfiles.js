3e588e51f4d7a095ac2fe8a939b6b19268668159
Sean Glover <sean.glover@akumina.com>
2019-07-17 12:55:02 -0400

    Update copyfiles.js

--- website/copyfiles.js
+++ website/copyfiles.js
@@ -1,5 +1,6 @@
 
 //Ref: https://gabrieleromanato.name/nodejs-renaming-files-recursively
+//Ref: http://nephewapps.com/2018/09/09/programmatically-edit-any-file-in-nodejs/
 'use strict';
 
 const path = require('path');
@@ -10,20 +11,25 @@ const listDir = (dir, fileList = []) => {
     let files = fs.readdirSync(dir);
 
     files.forEach(file => {
-
-        let name = file.replace(':', '');;
+        let name = file.replace(':', '');
+        let title = name.split('.md')[0];
         let src = path.join(dir, file);
         let newSrc = path.join(dir, name);
+        let oldContent = fs.readFileSync(path.join(dir, file), 'utf-8');
+        let newTitle = title.replace(/-/g, ' ')
         fileList.push({
             oldSrc: src,
-            newSrc: newSrc
+            newSrc: newSrc,
+            oldContent: oldContent,
+            newContent: `---\r\nid: ${title}\r\ntitle: ${newTitle}\r\n---\r\n\r\n`,
         });
     });
 
     return fileList;
 };
 
-let foundFiles = listDir('~/../AkuminaDev.wiki');
+let foundFiles = listDir('../docs');
 foundFiles.forEach(f => {
-    fs.renameSync(f.oldSrc, f.newSrc);
+    fs.unlinkSync(f.oldSrc);
+    fs.writeFileSync(f.newSrc, f.newContent + f.oldContent);
 });
